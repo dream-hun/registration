51721f5d5e6dab9f8d6c285bcb79908280f5964a
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-05-16 11:28:57 +0200

    added latest application

---
+++ app/Filament/Widgets/LatestApplications.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Filament\Resources\StudentResource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestApplications extends BaseWidget
+{
+    protected static int $sort = 2;
+
+    protected int|string|array $columnSpan = 'full';
+
+    public function table(Table $table): Table
+    {
+
+        return $table
+            ->query(StudentResource::getEloquentQuery())
+            ->defaultPaginationPageOption(10)
+            ->defaultSort('created_at', 'desc')
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('gender')
+                    ->badge()
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('department.name')
+                    ->searchable()
+                    ->sortable(),
+            ]);
+    }
+}
--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,9 +2,9 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Student;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use App\Models\Student;
 
 class StatsOverview extends BaseWidget
 {
@@ -14,13 +14,16 @@ class StatsOverview extends BaseWidget
             Stat::make('Total Students', Student::count())
                 ->description('Total number of all students in all trades')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->chart([7, 2, 10, 3, 15, 4, 20])
                 ->color('warning'),
-            Stat::make('Male students', Student::where('gender','=','male')->count())
+            Stat::make('Male students', Student::where('gender', '=', 'male')->count())
                 ->description('Total number of Male students')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('info'),
-            Stat::make('Female students', Student::where('gender','=','female')->count())
+            Stat::make('Female students', Student::where('gender', '=', 'female')->count())
                 ->description('Total number of Female students')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('success'),
         ];
